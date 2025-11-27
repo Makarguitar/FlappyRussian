2aa2fb63e5f0083ec6ef94097a49265f7ce1cbe4
makar <makmakhorin@hotmail.com>
2025-11-27 17:58:26 +0400

    column moves and destroys

--- Assets/Scripts/Column.cs
+++ Assets/Scripts/Column.cs
@@ -4,9 +4,12 @@ public class Column : MonoBehaviour
 {
     [SerializeField] Transform columnTop, columnBottom;
     [SerializeField] float minOffset, maxOffset;
-
+    [SerializeField] float speed = 5f;
+    GameManager gm;
+    
     void Start()
     {
+        gm = FindAnyObjectByType<GameManager>();
         float offset = Random.Range(minOffset, maxOffset) / 2;
         columnTop.position += new Vector3(0, offset, 0);
         columnBottom.position -= new Vector3(0, offset, 0);
@@ -14,6 +17,13 @@ public class Column : MonoBehaviour
 
     void Update()
     {
-        
+        if (gameObject.transform.position.x > gm.BottomLeft.x - gameObject.transform.localScale.x / 2)
+        {
+            gameObject.transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
